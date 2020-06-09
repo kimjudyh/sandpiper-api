70ecbf20343817fe859a9b4de80a937a9780d137
Judy <kimjudyh@gmail.com>
2020-06-09 11:16:37 -0700

    added some behaviors

--- seed.js
+++ seed.js
@@ -1,6 +1,12 @@
 const db = require('./models');
 const behaviorData = require('./behaviorData.json');
 
+// db.Behavior.update({}, behaviorData.behaviors, {upsert: true}, (err, seededBehaviors) => {
+//   if (err) console.log(err);
+//   else console.log(behaviorData.behaviors.length, 'behaviors created');
+//   process.exit();
+// })
+
 db.Behavior.deleteMany({}, (err, deletedBehaviors) => {
   db.Behavior.create(behaviorData.behaviors, (err, seededBehaviors) => {
     if (err) console.log(err);
